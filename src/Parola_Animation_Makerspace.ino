a32bce0d64be2807d11574bf2ae07845652984ad
unknown <mimuehl@gmail.com>
2018-10-27 14:33:56 +0200

    Code formated (Tab 2)

--- src/Parola_Animation_Makerspace.ino
+++ src/Parola_Animation_Makerspace.ino
@@ -44,223 +44,219 @@ boolean animation_enable = true;
 
 char lmessage[maxLines][numChars]=
 {
-        "MAKERSPACE",
-        "Wiesbaden e.V.",
-        "> > > > > >"
+  "MAKERSPACE",
+  "Wiesbaden e.V.",
+  "> > > > > >"
 };
 
 typedef struct
 {
-        uint16_t speed;   // speed multiplier of library default
-        uint16_t pause;   // pause multiplier for library default
-        uint16_t inFX;   // pause multiplier for library default
-        uint16_t outFX;   // pause multiplier for library default
+  uint16_t speed;   // speed multiplier of library default
+  uint16_t pause;   // pause multiplier for library default
+  uint16_t inFX;   // pause multiplier for library default
+  uint16_t outFX;   // pause multiplier for library default
 } sCatalog;
 
 sCatalog catalog[maxLines]
 {
-        {50, 1000, 6, 6 },
-        {50, 1000, 3, 3 },
-        {25, 0, 1, 1 },
-        {0, 0, 0, 0 },
-        {0, 0, 0, 0 },
-        {0, 0, 0, 0 }
+  {50, 1000, 6, 6 },
+  {50, 1000, 3, 3 },
+  {25, 0, 1, 1 },
+  {0, 0, 0, 0 },
+  {0, 0, 0, 0 },
+  {0, 0, 0, 0 }
 };
 
 textEffect_t effect[] =
 {
-        PA_PRINT,
-        PA_SCROLL_RIGHT,
-        PA_SCROLL_LEFT,
-        PA_SCROLL_UP_RIGHT,
-        PA_SCROLL_UP_LEFT,
-        PA_SCROLL_DOWN_RIGHT,
-        PA_SCROLL_DOWN_LEFT,
-        PA_SCROLL_UP,
-        PA_SCROLL_DOWN,
-        PA_GROW_UP,
-        PA_GROW_DOWN,
-        PA_OPENING,
-        PA_CLOSING,
-        PA_OPENING_CURSOR,
-        PA_WIPE,
-        PA_WIPE_CURSOR,
-        PA_MESH,
-        PA_RANDOM
+  PA_PRINT,
+  PA_SCROLL_RIGHT,
+  PA_SCROLL_LEFT,
+  PA_SCROLL_UP_RIGHT,
+  PA_SCROLL_UP_LEFT,
+  PA_SCROLL_DOWN_RIGHT,
+  PA_SCROLL_DOWN_LEFT,
+  PA_SCROLL_UP,
+  PA_SCROLL_DOWN,
+  PA_GROW_UP,
+  PA_GROW_DOWN,
+  PA_OPENING,
+  PA_CLOSING,
+  PA_OPENING_CURSOR,
+  PA_WIPE,
+  PA_WIPE_CURSOR,
+  PA_MESH,
+  PA_RANDOM
 };
 
 void setup()  {
-        Serial.begin(115200);
+  Serial.begin(115200);
 
-        P.begin();
-        P.setInvert(false);
-        inStr.reserve(numChars); // reserve for instr serial input
-        pinMode(LED_SW,OUTPUT);
-        digitalWrite(LED_SW,LOW);
+  P.begin();
+  P.setInvert(false);
+  inStr.reserve(numChars); // reserve for instr serial input
+  pinMode(LED_SW,OUTPUT);
+  digitalWrite(LED_SW,LOW);
 
-        /*
-           for (uint8_t i=0; i<L2D; i++)
-           {
-           catalog[i].speed *= P.getSpeed();
-           catalog[i].pause *= 500;
-           }
-         */
+/*
+   for (uint8_t i=0; i<L2D; i++)
+   {
+   catalog[i].speed *= P.getSpeed();
+   catalog[i].pause *= 500;
+   }
+*/
 }
 
 void recvWithStartEndMarkers() {
-        static boolean recvInProgress = false;
-        static int ndx = 0;
-        char startMarker = '<';
-        char endMarker = '>';
-        char rc;
+  static boolean recvInProgress = false;
+  static int ndx = 0;
+  char startMarker = '<';
+  char endMarker = '>';
+  char rc;
 
-        while (Serial.available() > 0 && newData == false) {
-                rc = Serial.read();
-                if (recvInProgress == true) {
-                        if (rc != endMarker) {
-                                receivedChars[ndx] = rc;
-                                ndx++;
-                                if (ndx >= numChars) {
-                                        ndx = numChars - 1;
-                                }
-                        }
-                        else {
-                                receivedChars[ndx] = '\0'; // terminate the string
-                                recvInProgress = false;
-                                ndx = 0;
-                                newData = true;
-                        }
-                }
+  while (Serial.available() > 0 && newData == false) {
+    rc = Serial.read();
+    if (recvInProgress == true) {
+      if (rc != endMarker) {
+        receivedChars[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars) {
+                ndx = numChars - 1;
+        }
+      }
+      else {
+        receivedChars[ndx] = '\0'; // terminate the string
+        recvInProgress = false;
+        ndx = 0;
+        newData = true;
+      }
+    }
 
-                else if (rc == startMarker) {
-                        recvInProgress = true;
-                }
-        }
-        if (newData == true) {
-                strcpy(tempChars, receivedChars);
-                parseData();
-                // showParsedData();
-                processParsedData();
-                newData = false;
-        }
+    else if (rc == startMarker) {
+      recvInProgress = true;
+    }
+  }
+  if (newData == true) {
+          strcpy(tempChars, receivedChars);
+          parseData();
+          // showParsedData();
+          processParsedData();
+          newData = false;
+  }
 }
 
 void processParsedData() {
-        inStr = message;
-        if(line == 0) // Normal commands <0,command,0,0,0,0>
-        {
-                if (inStr.substring(0,4) == "STOP") { //
-                        animation_enable = false;
-                        P.displayClear();
-                        Serial.println("CMD;STOP;COMPLETED");
-                }
+  inStr = message;
+  if(line == 0) // Normal commands <0,command,0,0,0,0>
+  {
+    if (inStr.substring(0,4) == "STOP") { //
+            animation_enable = false;
+            P.displayClear();
+            Serial.println("CMD;STOP;COMPLETED");
+    }
 
-                if (inStr.substring(0,5) == "START") { //
-                        animation_enable = true;
-                        Serial.println("CMD;START;COMPLETED");
-                }
+    if (inStr.substring(0,5) == "START") { //
+            animation_enable = true;
+            Serial.println("CMD;START;COMPLETED");
+    }
 
-                if (inStr.substring(0,5) == "INIT") { // Clear all Text Buffer
-                        asm volatile ("jmp 0");
-                        Serial.println("CMD;INIT;COMPLETED");
-                }
+    if (inStr.substring(0,5) == "INIT") { // Clear all Text Buffer
+            asm volatile ("jmp 0");
+            Serial.println("CMD;INIT;COMPLETED");
+    }
 
-                if (inStr.substring(0,3) == "LON") { // Turn LED Lighting on
-                        digitalWrite(LED_SW,HIGH);
-                        Serial.println("CMD;LON;COMPLETED");
-                }
+    if (inStr.substring(0,3) == "LON") { // Turn LED Lighting on
+            digitalWrite(LED_SW,HIGH);
+            Serial.println("CMD;LON;COMPLETED");
+    }
 
-                if (inStr.substring(0,4) == "LOFF") { // Turn LED Lighting off
-                        digitalWrite(LED_SW,LOW);
-                        Serial.println("CMD;LOFF;COMPLETED");
-                }
+    if (inStr.substring(0,4) == "LOFF") { // Turn LED Lighting off
+            digitalWrite(LED_SW,LOW);
+            Serial.println("CMD;LOFF;COMPLETED");
+    }
 
-                if (inStr.substring(0,4) == "L2ON") { // Turn LED Lighting on
-                        digitalWrite(L2_SW,HIGH);
-                        Serial.println("CMD;L2ON;COMPLETED");
-                }
+    if (inStr.substring(0,4) == "L2ON") { // Turn LED Lighting on
+            digitalWrite(L2_SW,HIGH);
+            Serial.println("CMD;L2ON;COMPLETED");
+    }
 
-                if (inStr.substring(0,5) == "L2OFF") { // Turn LED Lighting off
-                        digitalWrite(L2_SW,LOW);
-                        Serial.println("CMD;L2OFF;COMPLETED");
-                }
+    if (inStr.substring(0,5) == "L2OFF") { // Turn LED Lighting off
+            digitalWrite(L2_SW,LOW);
+            Serial.println("CMD;L2OFF;COMPLETED");
+    }
 
-        }
-        else                        // Display commands
-        {
-                uint8_t p = line-1;
-                inStr.toCharArray(lmessage[p], numChars);
-                catalog[p].inFX = inFX;
-                catalog[p].outFX = outFX;
-                catalog[p].speed = dspeed;
-                catalog[p].pause = pause;
-
-        }
+  }
+  else                        // Display commands
+  {
+    uint8_t p = line-1;
+    inStr.toCharArray(lmessage[p], numChars);
+    catalog[p].inFX = inFX;
+    catalog[p].outFX = outFX;
+    catalog[p].speed = dspeed;
+    catalog[p].pause = pause;
+  }
 }
 
 void parseData() {      // split the data into its parts
+  char * strtokIndx; // this is used by strtok() as an index
 
-        char * strtokIndx; // this is used by strtok() as an index
+  strtokIndx = strtok(tempChars,",");  // get the first part - the string
+  line = atoi(strtokIndx);
 
-        strtokIndx = strtok(tempChars,",");  // get the first part - the string
-        line = atoi(strtokIndx);
+  strtokIndx = strtok(NULL, ",");
+  strcpy(message, strtokIndx);
 
-        strtokIndx = strtok(NULL, ",");
-        strcpy(message, strtokIndx);
+  strtokIndx = strtok(NULL, ",");      // this continues where the previous call left off
+  dspeed = atoi(strtokIndx);           // convert this part to an integer
 
-        strtokIndx = strtok(NULL, ",");      // this continues where the previous call left off
-        dspeed = atoi(strtokIndx);           // convert this part to an integer
+  strtokIndx = strtok(NULL, ",");
+  pause = atoi(strtokIndx);     // convert this part to an integer
 
-        strtokIndx = strtok(NULL, ",");
-        pause = atoi(strtokIndx);     // convert this part to an integer
+  strtokIndx = strtok(NULL, ",");      // this continues where the previous call left off
+  inFX = atoi(strtokIndx);    // convert this part to an integer
 
-        strtokIndx = strtok(NULL, ",");      // this continues where the previous call left off
-        inFX = atoi(strtokIndx);    // convert this part to an integer
-
-        strtokIndx = strtok(NULL, ",");      // this continues where the previous call left off
-        outFX = atoi(strtokIndx);    // convert this part to an integer
-
+  strtokIndx = strtok(NULL, ",");      // this continues where the previous call left off
+  outFX = atoi(strtokIndx);    // convert this part to an integer
 }
 
 void showParsedData() {
-        Serial.println(tempChars);
-        Serial.print("Line ");
-        Serial.println(line);
-        Serial.print("Effect In ");
-        Serial.println(inFX);
-        Serial.print("Effect Out ");
-        Serial.println(outFX);
-        Serial.print("Message ");
-        Serial.println(message);
-        Serial.print("speed ");
-        Serial.println(dspeed);
-        Serial.print("Pause ");
-        Serial.println(pause);
-
+  Serial.println(tempChars);
+  Serial.print("Line ");
+  Serial.println(line);
+  Serial.print("Effect In ");
+  Serial.println(inFX);
+  Serial.print("Effect Out ");
+  Serial.println(outFX);
+  Serial.print("Message ");
+  Serial.println(message);
+  Serial.print("speed ");
+  Serial.println(dspeed);
+  Serial.print("Pause ");
+  Serial.println(pause);
 }
 
 void loop() {
-        recvWithStartEndMarkers(); // look for any commands
-        if(animation_enable == true) {
-                P.displayReset();
-                P.displayClear();
-                textPosition_t just;
-                just = PA_CENTER;
-                for (uint8_t i=0; i<L2D; i++)
-                {
-                        Serial.print("L"); Serial.print(i+1); Serial.print(";");
-                        Serial.print(lmessage[i]); Serial.print(";");
-                        Serial.print(catalog[i].speed); Serial.print(";");
-                        Serial.print(catalog[i].pause); Serial.print(";");
-                        Serial.print(catalog[i].inFX); Serial.print(";");
-                        Serial.println(catalog[i].outFX);
+  recvWithStartEndMarkers(); // look for any commands
+  if(animation_enable == true) {
+    P.displayReset();
+    P.displayClear();
+    textPosition_t just;
+    just = PA_CENTER;
+    for (uint8_t i=0; i<L2D; i++)
+    {
+      Serial.print("L"); Serial.print(i+1); Serial.print(";");
+      Serial.print(lmessage[i]); Serial.print(";");
+      Serial.print(catalog[i].speed); Serial.print(";");
+      Serial.print(catalog[i].pause); Serial.print(";");
+      Serial.print(catalog[i].inFX); Serial.print(";");
+      Serial.println(catalog[i].outFX);
 
-                        P.displayText(lmessage[i], just, catalog[i].speed, catalog[i].pause, effect[catalog[i].inFX], effect[catalog[i].outFX]);
-                        while (!P.displayAnimate())
-                                ; // animates and returns true when an animation is completed
-                        if(animation_enable == false) return;
-                        delay(catalog[i].pause);
-                        recvWithStartEndMarkers(); // look for any commands
-                }
-        }
+      P.displayText(lmessage[i], just, catalog[i].speed, catalog[i].pause, effect[catalog[i].inFX], effect[catalog[i].outFX]);
+      while (!P.displayAnimate())
+              ; // animates and returns true when an animation is completed
+      if(animation_enable == false) return;
+      delay(catalog[i].pause);
+      recvWithStartEndMarkers(); // look for any commands
+    }
+  }
 }

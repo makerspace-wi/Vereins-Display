3fa3078686d1a2b9736f78695e0e69dd592258db
Michael Muehl <mimuehl@gmail.com>
2020-01-26 14:24:37 +0100

    Less communication on xBee

--- src/Parola_Animation_Makerspace.ino
+++ src/Parola_Animation_Makerspace.ino
@@ -1,33 +1,62 @@
+/* DESCRIPTION
+  ====================
+  started on 01JAN2017 - uploaded on 06.01.2017 by Dieter
+
+  Commands to Raspi --->
+  'OK'  - message received
+
+  Commands from Raspi <--- <0,command>
+  'DN'  - Display once
+  'DF'  - Display ofF
+  'INI' - Initialize all new
+  'L1N' - Light 1 oN
+  'L1F' - Light 1 ofF
+  'L2N' - Light 2 oN
+  'L2F' - Light 2 ofF
+
+    <--- <1,Text,0,0,0,0> show text in (line - 1) until maxLines
+
+  last change: 26.01.2020 by Michael Muehl
+  changed: reduse bus communication nearly zero form display,
+           and POR; and Version
+*/
+#define Version "1.2.x" // (Test =1.2.x ==> 1.2.1)
+
+#include <Arduino.h>
 #include <MD_Parola.h>
 #include <MD_MAX72xx.h>
 #include <SPI.h>
 
+// PIN Assignments
 // Define the number of devices we have in the chain and the hardware interface
 // NOTE: These pin numbers will probably not work with your hardware and may
 // need to be adapted
-#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
-#define MAX_DEVICES 16
-#define CLK_PIN   13  // blau -> Bulgin 3
-#define DATA_PIN  11  // grün -> Bulgin 1
+#define CLK_PIN   13  // blau    -> Bulgin 3
+#define DATA_PIN  11  // grün    -> Bulgin 1
 #define CS_PIN    10  // violett -> Bulgin 2
 #define LED_SW1   A2  // A2 auf 2 LED Display SW1
-#define not_SW2   A3  // A3 auf 3 [not used]  SW2
+#define LED_SW2   A3  // A3 auf 3 [not used]  SW2
 
 // Hardware SPI connection
+#define MAX_DEVICES 16
+#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 MD_Parola P = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 
+// DEFINES
 #define ARRAY_SIZE(x)  (sizeof(x)/sizeof(x[0]))
 #define SPEED_TIME  25
 #define PAUSE_TIME  1000
+
+// VARIABLES
 const byte numChars = 50;
 const byte maxLines = 6;
 
 String inStr = "";      // a string to hold incoming data
 
-uint8_t L2D = 6;     // Lines to Display
+uint8_t L2D = 6;        // Lines to Display
 char receivedChars[numChars];
 char input[numChars];
-char tempChars[numChars];        // temporary array for use when parsing
+char tempChars[numChars]; // temporary array for use when parsing
 // variables to hold the parsed data
 char message[numChars] = {0};
 uint16_t line = 0;
@@ -40,7 +69,6 @@ boolean newData = false;
 boolean animation_enable = true;
 
 // Global data
-
 char lmessage[maxLines][numChars]=
 {
   "MAKERSPACE",
@@ -50,10 +78,10 @@ char lmessage[maxLines][numChars]=
 
 typedef struct
 {
-  uint16_t speed;   // speed multiplier of library default
-  uint16_t pause;   // pause multiplier for library default
+  uint16_t speed;  // speed multiplier of library default
+  uint16_t pause;  // pause multiplier for library default
   uint16_t inFX;   // pause multiplier for library default
-  uint16_t outFX;   // pause multiplier for library default
+  uint16_t outFX;  // pause multiplier for library default
 } sCatalog;
 
 sCatalog catalog[maxLines]
@@ -88,17 +116,28 @@ textEffect_t effect[] =
   PA_RANDOM
 };
 
-void setup()  {
+// ======>  SET UP AREA <=====
+void setup() {
+  //init Serial port
   Serial.begin(115200);
+  inStr.reserve(numChars); // reserve for instr serial input
+
+  // initialize:
   P.begin();
   P.setInvert(false);
-  inStr.reserve(numChars); // reserve for instr serial input
-  pinMode(LED_SW1,OUTPUT);
-  digitalWrite(LED_SW1,LOW);
-  pinMode(not_SW2,OUTPUT);
-  digitalWrite(not_SW2,LOW);
-}
 
+  // IO MODES
+  pinMode(LED_SW1,OUTPUT);
+  pinMode(LED_SW2,OUTPUT);
+
+  // Set default values
+  digitalWrite(LED_SW1,LOW);
+  digitalWrite(LED_SW2,LOW);
+  Serial.println(String("MAKERDISP") + ";POR" + ";V" + Version);
+}
+// Setup End -----------------------------------
+
+// FUNCTIONS ------------------------------------
 void recvWithStartEndMarkers() {
   static boolean recvInProgress = false;
   static int ndx = 0;
@@ -113,16 +152,13 @@ void recvWithStartEndMarkers() {
         receivedChars[ndx] = rc;
         ndx++;
         if (ndx >= numChars) ndx = numChars - 1;
-      }
-      else {
+      } else {
         receivedChars[ndx] = '\0'; // terminate the string
         recvInProgress = false;
         ndx = 0;
         newData = true;
       }
-    }
-
-    else if (rc == startMarker) {
+    } else if (rc == startMarker) {
       recvInProgress = true;
     }
   }
@@ -137,47 +173,37 @@ void recvWithStartEndMarkers() {
 
 void processParsedData() {
   inStr = message;
-  if(line == 0) // Normal commands <0,command,0,0,0,0>
-  {
-    if (inStr.substring(0,4) == "STOP") { //
+  if (line == 0) { // Normal commands <0,command,0,0,0,0>
+    if (inStr.substring(0,2) == "DN") { // Display oN
+      animation_enable = true;
+    }
+
+    if (inStr.substring(0,2) == "DF") { // Display ofF
       animation_enable = false;
       P.displayClear();
-      Serial.println("CMD;STOP;COMPLETED");
     }
 
-    if (inStr.substring(0,5) == "START") { //
-      animation_enable = true;
-      Serial.println("CMD;START;COMPLETED");
-    }
-
-    if (inStr.substring(0,5) == "INIT") { // Clear all Text Buffer
+    if (inStr.substring(0,3) == "INI") { // Initialize all new
       asm volatile ("jmp 0");
-      Serial.println("CMD;INIT;COMPLETED");
     }
 
-    if (inStr.substring(0,3) == "LON") { // Turn LED Lighting on
+    if (inStr.substring(0,3) == "L1N") { // Turn LED Light 1 oN
       digitalWrite(LED_SW1,HIGH);
-      Serial.println("CMD;LON;COMPLETED");
     }
 
-    if (inStr.substring(0,4) == "LOFF") { // Turn LED Lighting off
+    if (inStr.substring(0,3) == "L1F") { // Turn LED Light 1 ofF
       digitalWrite(LED_SW1,LOW);
-      Serial.println("CMD;LOFF;COMPLETED");
     }
 
-    if (inStr.substring(0,4) == "L2ON") { // Turn LED Lighting on
-      digitalWrite(not_SW2,HIGH);
-      Serial.println("CMD;L2ON;COMPLETED");
+    if (inStr.substring(0,3) == "L2N") { // Turn LED Light 2 oN
+      digitalWrite(LED_SW2,HIGH);
     }
 
-    if (inStr.substring(0,5) == "L2OFF") { // Turn LED Lighting off
-      digitalWrite(not_SW2,LOW);
-      Serial.println("CMD;L2OFF;COMPLETED");
+    if (inStr.substring(0,3) == "L2F") { // Turn LED Light 2 ofF
+      digitalWrite(LED_SW2,LOW);
     }
 
-  }
-  else                        // Display commands
-  {
+  } else {                       // Display commands
     uint8_t p = line-1;
     inStr.toCharArray(lmessage[p], numChars);
     catalog[p].inFX = inFX;
@@ -208,39 +234,17 @@ void parseData() {      // split the data into its parts
   strtokIndx = strtok(NULL, ",");      // this continues where the previous call left off
   outFX = atoi(strtokIndx);    // convert this part to an integer
 }
+// End FUNCTIONS --------------------------------
 
-void showParsedData() {
-  Serial.println(tempChars);
-  Serial.print("Line ");
-  Serial.println(line);
-  Serial.print("Effect In ");
-  Serial.println(inFX);
-  Serial.print("Effect Out ");
-  Serial.println(outFX);
-  Serial.print("Message ");
-  Serial.println(message);
-  Serial.print("speed ");
-  Serial.println(dspeed);
-  Serial.print("Pause ");
-  Serial.println(pause);
-}
-
+// Loop -----------------------------------------
 void loop() {
   recvWithStartEndMarkers(); // look for any commands
-  if(animation_enable == true) {
+  if (animation_enable == true) {
     P.displayReset();
     P.displayClear();
     textPosition_t just;
     just = PA_CENTER;
-    for (uint8_t i=0; i<L2D; i++)
-    {
-      Serial.print("L"); Serial.print(i+1); Serial.print(";");
-      Serial.print(lmessage[i]); Serial.print(";");
-      Serial.print(catalog[i].speed); Serial.print(";");
-      Serial.print(catalog[i].pause); Serial.print(";");
-      Serial.print(catalog[i].inFX); Serial.print(";");
-      Serial.println(catalog[i].outFX);
-
+    for (uint8_t i=0; i<L2D; i++) {
       P.displayText(lmessage[i], just, catalog[i].speed, catalog[i].pause, effect[catalog[i].inFX], effect[catalog[i].outFX]);
       while (!P.displayAnimate())
               ; // animates and returns true when an animation is completed
